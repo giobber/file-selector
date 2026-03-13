375f9fca7aa9918e72c3d8c9c436a79b9c5d1073
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2026-03-13 14:07:58 +0100

    Add logger

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,14 @@
+from typing import Literal
+
+from loguru import logger
 from pydantic_settings import BaseSettings
+
+LogLevel = Literal["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
 
 
 class Settings(BaseSettings):
     DEBUG: bool = False
+    LOG_LEVEL: LogLevel | int = "INFO"
 
     class Config:
         env_file = ".env"
@@ -11,3 +17,13 @@ class Settings(BaseSettings):
 
 def get_settings():
     return Settings()
+
+
+def log_settings():
+    settings = get_settings()
+
+    if not settings.DEBUG:
+        return
+
+    for key, value in get_settings():
+        logger.info(f"Settings.{key}={value}")
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import sys
+
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from loguru import logger
 
-from config.settings import Settings, get_settings
+from config.settings import Settings, get_settings, log_settings
 
+# Setup logger
+logger.remove()
+logger.add(sys.stderr, level=get_settings().LOG_LEVEL)
+log_settings()
+
+# Initialize FastAPI app
 app = FastAPI()
 
 # Set up Jinja2 templates
@@ -12,7 +21,6 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request, settings: Settings = Depends(get_settings)):
-    print(settings)
     return templates.TemplateResponse(
         "index.html", {"request": request, "debug": settings.DEBUG}
     )
